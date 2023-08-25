bb212086fee400e2a550a0f4232249bafba5a425
SamirMohamed <samirmohamed594@gmail.com>
2023-08-25 08:01:18 +0300

    feat: add api for getting number of packages per size

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"gymshark/store"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hi")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/store/packs", getNumOfPacks)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func getNumOfPacks(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		NumOfItems int `json:"num_of_items"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	packsSizes := getPacksSizes()
+
+	numOfPacks := store.GetNumOfPacks(packsSizes, body.NumOfItems)
+	res, err := json.Marshal(numOfPacks)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, string(res))
+
+}
+
+func getPacksSizes() []int {
+	// Dummy packs size data
+	return []int{250, 500, 1000, 2000, 5000}
 }
